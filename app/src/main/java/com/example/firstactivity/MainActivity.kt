83c6db2601923734a4350d525b0d7ba277f85421
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-03 14:13:27 +0100

    use button to show and hide text

--- app/src/main/java/com/example/firstactivity/MainActivity.kt
+++ app/src/main/java/com/example/firstactivity/MainActivity.kt
@@ -3,28 +3,31 @@ package com.example.firstactivity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var myText : TextView;
+    lateinit var myButton : Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         var clicked = false;
         myText = findViewById(R.id.myText);
-        myText.setText("This is a text setted by kotlin!");
-        myText.setOnClickListener{
-            if(!clicked){
-                myText.setText("You clicked me");
-                myText.setTextColor(Color.WHITE);
-                myText.setBackgroundColor(Color.BLACK);
-                clicked=true;
+        myText.visibility = View.INVISIBLE;
+        myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener {
+            if (!clicked){
+                myText.visibility = View.VISIBLE;
+                myButton.text = "Hide the text"
+                clicked = true;
             }else{
-                myText.setText("You unclicked me");
-                myText.setTextColor(Color.RED);
-                myText.setBackgroundColor(Color.BLUE);
+                myText.visibility = View.INVISIBLE;
+                myButton.text = "Show text";
                 clicked=false;
             }
         }
+
     }
 }
