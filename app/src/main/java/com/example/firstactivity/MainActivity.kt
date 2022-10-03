24b24d21a02b7ddbf2320bd1a24d02cfc6878431
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-03 15:08:43 +0100

    use and change image

--- app/src/main/java/com/example/firstactivity/MainActivity.kt
+++ app/src/main/java/com/example/firstactivity/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.firstactivity
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     lateinit var myText : TextView;
     lateinit var myButton : Button;
     lateinit var myEditText : EditText;
+    lateinit var myImage : ImageView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myText = findViewById(R.id.myText);
         myButton = findViewById(R.id.myButton);
         myEditText = findViewById(R.id.myEditText);
+        myImage = findViewById(R.id.myImage);
+        myImage.isVisible = false;
         myButton.setOnClickListener {
+            myImage.isVisible=true;
             if (myEditText.text.length != 0){
                 myText.text = "Hello "+myEditText.text;
             }else{
                 myText.text = "Hello DSI31";
+                myImage.setImageResource(R.drawable.img2);
             }
         }
 
